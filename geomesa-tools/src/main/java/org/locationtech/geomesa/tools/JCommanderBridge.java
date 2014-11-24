4c36d49db383180b4e0c1f833ea67977bb61ea5e
Andrew Hulbert <ahulbert@ccri.com>
2014-11-24 18:36:57 -0500

    removing unused java stuff

--- geomesa-tools/src/main/java/org/locationtech/geomesa/tools/JCommanderBridge.java
+++
@@ -1,9 +0,0 @@
-//package org.locationtech.geomesa.tools;
-//
-//import com.beust.jcommander.JCommander;
-//
-//public class JCommanderBridge {
-//    public static JCommander create(Object argsObject) {
-//        return new JCommander(argsObject);
-//    }
-//}
