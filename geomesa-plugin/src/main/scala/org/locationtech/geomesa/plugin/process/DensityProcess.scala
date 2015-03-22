ad013c8888a1663dd49701bb06184874e200374b
Jim Hughes <jnh5y@ccri.com>
2015-03-22 19:34:53 -0400

    Quick rewrite of GM heatmap process

--- geomesa-plugin/src/main/scala/org/locationtech/geomesa/plugin/process/DensityProcess.scala
+++ geomesa-plugin/src/main/scala/org/locationtech/geomesa/plugin/process/DensityProcess.scala
@@ -17,12 +17,21 @@
 
 package org.locationtech.geomesa.plugin.process
 
+import org.geotools.coverage.CoverageFactoryFinder
+import org.geotools.coverage.grid.GridCoverage2D
 import org.geotools.data.Query
+import org.geotools.data.simple.SimpleFeatureCollection
+import org.geotools.factory.GeoTools
 import org.geotools.geometry.jts.ReferencedEnvelope
 import org.geotools.process.factory.{DescribeParameter, DescribeProcess}
-import org.geotools.process.vector.HeatmapProcess
+import org.geotools.process.vector.{HeatmapProcess, HeatmapSurface}
 import org.locationtech.geomesa.core.index.QueryHints
+import org.locationtech.geomesa.core.iterators.DensityIterator._
 import org.opengis.coverage.grid.GridGeometry
+import org.opengis.util.ProgressListener
+
+import scala.collection.JavaConversions._
+import scala.util.{Failure, Success, Try}
 
 @DescribeProcess(
   title = "Density Map",
@@ -48,5 +57,78 @@ class DensityProcess extends HeatmapProcess {
     q.getHints.put(QueryHints.WIDTH_KEY, argOutputWidth)
     q.getHints.put(QueryHints.HEIGHT_KEY, argOutputHeight)
     q
+  }
+
+  override def execute(@DescribeParameter(name = "data", description = "Input features") obsFeatures: SimpleFeatureCollection,
+                       @DescribeParameter(name = "radiusPixels", description = "Radius of the density kernel in pixels") argRadiusPixels: Integer,
+                       @DescribeParameter(name = "weightAttr", description = "Name of the attribute to use for data point weight", min = 0, max = 1) valueAttr: String,
+                       @DescribeParameter(name = "pixelsPerCell", description = "Resolution at which to compute the heatmap (in pixels). Default = 1", defaultValue="1", min = 0, max = 1) argPixelsPerCell: Integer,
+                       @DescribeParameter(name = "outputBBOX", description = "Bounding box of the output") argOutputEnv: ReferencedEnvelope,
+                       @DescribeParameter(name = "outputWidth", description = "Width of output raster in pixels") argOutputWidth: Integer,
+                       @DescribeParameter(name = "outputHeight", description = "Height of output raster in pixels") argOutputHeight: Integer,
+                       monitor: ProgressListener): GridCoverage2D = {
+
+    val pixelsPerCell: Int = if (argPixelsPerCell != null && argPixelsPerCell > 1) argPixelsPerCell
+    else 1
+    val outputWidth = argOutputWidth.toInt
+    val outputHeight = argOutputHeight.toInt
+
+    val (gridWidth: Int, gridHeight: Int) = if (pixelsPerCell > 1) (outputWidth / pixelsPerCell, outputHeight/ pixelsPerCell)
+
+    // TODO: Correctly handle transformations
+    //    val srcCRS = obsFeatures.getSchema.getCoordinateReferenceSystem
+    //    val dstCRS = argOutputEnv.getCoordinateReferenceSystem
+    //    val trans = CRS.findMathTransform(srcCRS, dstCRS)
+
+    val radiusCells: Int =
+      if (argRadiusPixels != null) argRadiusPixels / pixelsPerCell
+      else                         100 / pixelsPerCell
+
+    val hms = new HeatmapSurface(radiusCells, argOutputEnv, gridWidth, gridHeight)
+
+    try {
+      extractPoints(obsFeatures, valueAttr, hms)
+    }
+
+    val hmg = flipXY(hms.computeSurface)
+
+    // Handle upsampling Later
+
+    val gcf = CoverageFactoryFinder.getGridCoverageFactory(GeoTools.getDefaultHints())
+
+    gcf.create("Process Results", hmg, argOutputEnv)
+  }
+
+  def extractPoints(obsFeatures: SimpleFeatureCollection, valueAttr: String, hms: HeatmapSurface) {
+    val obsIt = obsFeatures.features()
+
+    while (obsIt.hasNext) {
+      val sf = obsIt.next()
+      val decodedMap = Try(decodeSparseMatrix(sf.getAttribute(ENCODED_RASTER_ATTRIBUTE).toString))
+
+      decodedMap match {
+        case Success(raster) =>
+          for {
+            (latIdx, col)   <- raster.rowMap
+            (lonIdx, count) <- col
+          } hms.addPoint(lonIdx, latIdx, count)
+
+        case Failure(e) =>
+          println(s"Error expanding encoded raster ${sf.getAttribute(ENCODED_RASTER_ATTRIBUTE)}: ${e.toString}", e)
+      }
+    }
+  }
+
+  def flipXY(grid: Array[Array[Float]]): Array[Array[Float]] = {
+    val xsize = grid.length
+    val ysize = grid(0).length
+
+    val grid2 = Array.ofDim[Float](ysize, xsize)
+
+    for {
+      i <- 0 until xsize
+      j <- 0 until ysize
+    } grid2(j)(i) = grid(i)(j)
+    grid2
   }
 }
